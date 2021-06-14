cdca186e202aafe717c0a97f805988cb558d255a
Walid CHEIKH <walid.cheikh.85@gmail.com>
2021-06-14 02:24:28 +0200

    Upgraded project to latest Spring boot 2.5.1 and fixed associated problems

--- src/main/java/training/spring/profilemanger/model/User.java
+++ src/main/java/training/spring/profilemanger/model/User.java
@@ -3,7 +3,14 @@ package training.spring.profilemanger.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
